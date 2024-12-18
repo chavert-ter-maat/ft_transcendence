52ca01cf7b302f01c9df0651c639f097f5de4354
Joost Zeeuw Van Der Laan <jzeeuw-v@f1r3s9.codam.nl>
2024-12-18 17:43:58 +0100

    remove old stuff

--- backend/src/auth/guards/passport.guard.ts
+++
@@ -1,7 +0,0 @@
-// src/auth/guards/passport.guard.ts
-
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class FortyTwoAuthGuard extends AuthGuard('42') {} //
