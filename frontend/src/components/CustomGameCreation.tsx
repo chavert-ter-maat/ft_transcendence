6ef8352c9750469dcb059e28a5a322c55409b595
Martin <martinhaan91+git@gmail.com>
2025-03-14 10:24:26 +0100

    adding custom game creating

--- frontend/src/components/CustomGameCreation.tsx
+++ frontend/src/components/CustomGameCreation.tsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import "./CustomGameCreation.css";
+import {
+  CustomGameModes,
+  CustomGameCreationProps,
+} from "../types/customGame.types";
 
-interface CustomGameProps {
-  userId: string;
-}
+const CustomGameCreation: React.FC<CustomGameCreationProps> = ({
+  onGameStart,
+  setQueueStatus,
+}) => {
+  const [gameSettings, setGameSettings] = useState({
+    gameMode: CustomGameModes,
+  });
+  // const [selectedMode, setSelectedMode] = useState<CustomGameModes | null>(
+  //   null
+  // );
 
-const CustomGameCreation: React.FC<CustomGameProps> = ({ userId }) => {
-  return <div className="customgame"></div>;
+  const handleModeSelect = (mode: CustomGameModes) => {
+    setSelectedMode(mode);
+  };
+
+  const handleStartGame = () => {
+    if (!selectedMode) return;
+
+    const gameMode =
+      selectedMode === "local-multiplayer" ? "localMultiplayer" : selectedMode;
+    setQueueStatus("inactive");
+    onGameStart(gameMode, "");
+  };
+
+  return (
+    <div className="custom-game-creation">
+      <h2 className="custom-game_title">Create Custom Game</h2>
+      <div className="settings">
+        <div className="setting">
+          <label>Paddle Size:</label>
+          <select
+            value={selectedMode || ""}
+            onChange={(e) => setSelectedMode(e.target.value as CustomGameModes)}
+          >
+            <option value="singleplayer">Singleplayer</option>
+            <option value="local multiplayer">Local Multiplayer</option>
+          </select>
+        </div>
+      </div>
+      <div className="buttons">
+        <button onClick={handleStartGame}>Start Game</button>
+        <button onClick={onBack}>Back to Lobby</button>
+      </div>
+    </div>
+  );
 };
 
 export default CustomGameCreation;
